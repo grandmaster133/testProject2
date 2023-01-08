bc4c0c7c5f681c08a49a3c4351b0635f09da2728
CoolMan <grandmaster9167@gmail.com>
2023-01-08 11:40:52 +0100

    added IO TS

--- typesFIle.ts
+++ typesFIle.ts
@@ -1,3 +1,5 @@
+import * as t from 'io-ts'
+
 let firstName: string = "Mike";
 var age: number = 30;
 var isCool: boolean = true;
@@ -5,3 +7,11 @@ var isCool: boolean = true;
 let secondname: string = "Cope";
 let experience: number = 10;
 var isSuper: boolean = false;
+
+const User =t.type({
+    userName: t.string,
+    userNumber: t.number
+})
+
+var a = User.props.userName;
+
